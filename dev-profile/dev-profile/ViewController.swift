85b37efdb680a772c47ec4b304b358e3d9e3f922
daramfon <Daramfon@buffalo.edu>
2019-07-08 13:52:42 -0400

    First Commit

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Daramfon Akpan on 7/8/19.
+//  Copyright © 2019 Daramfon Akpan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
